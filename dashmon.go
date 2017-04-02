ce10a9e4d78aca5afaa795a6b6cecaff694b88c5
Marc Chauffour <karmoc@gmail.com>
2017-04-02 11:59:35 +0200

    Test HTTP server

--- dashmon.go
+++ dashmon.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func status(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return
+	}
+	io.WriteString(w, "<h1>Status server</h1><h2>"+hostname+"</h2>")
+}
+
+var mux map[string]func(http.ResponseWriter, *http.Request)
 
 func main() {
-fmt.Printf("Hello, world.\n")
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: &myHandler{},
+	}
+
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/"] = hello
+	mux["/status"] = status
+
+	// fmt.Printf("Listening on localhost%s", server.Addr)
+	log.Fatal(server.ListenAndServe())
+}
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := mux[r.URL.String()]; ok {
+		h(w, r)
+		return
+	}
+
+	io.WriteString(w, "My server: "+r.URL.String())
 }
