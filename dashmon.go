2cb4c1ce4a10a4af8c9b9f306ff1cd1c42deab19
Marc Chauffour <karmoc@gmail.com>
2017-04-03 12:25:48 +0200

    Ajout vérification d'adresses sur DTC Mop
    Attention, mode ICMP nécessite sudo ou "as administrator"

--- dashmon.go
+++ dashmon.go
@@ -1,22 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/tatsushid/go-fastping"
 )
+
+// Get preferred outbound ip of this machine
+func getOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().String()
+	idx := strings.LastIndex(localAddr, ":")
+
+	return localAddr[0:idx]
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
+	var dtcStatus string
 	hostname, err := os.Hostname()
 	if err != nil {
 		return
 	}
-	io.WriteString(w, "<h1>Status server</h1><h2>"+hostname+"</h2>")
+	pingDTC := checkHostByIP("frmonsharfls01.emea.brinksgbl.com")
+	if pingDTC != 0 {
+		dtcStatus = pingDTC.String()
+	}
+	io.WriteString(w, "<div class='statushost'>"+hostname+"</div><div class='statusip'>"+getOutboundIP()+"</div><div class='statusdtc'>"+dtcStatus+"</div>")
+}
+
+func checkHostByIP(target string) time.Duration {
+	var outrtt time.Duration
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", target)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		// fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+		outrtt = rtt
+	}
+	p.OnIdle = func() {
+		// fmt.Println("finish")
+		// fmt.Printf("exit with RTT: %v\n", outrtt)
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return outrtt
 }
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
